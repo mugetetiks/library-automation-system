ed0bbf54dc6c98927a6c1e869dd414fcdaddb57a
mugetetik <mugetetik00@gmail.com>
2024-05-23 14:57:36 +0300

    changed style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,22 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Navbar className="header">
-      <Navbar.Brand href="/">Narnia Library</Navbar.Brand>
+    <Navbar className="header" expand="lg">
+      <Navbar.Brand href="/">Library System</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/books">Books</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto">
+          <Nav.Link href="/login" className='login-signup-link'>
+            <i className="bi bi-person-fill"></i> Log in
+          </Nav.Link>
+          <Nav.Link href="/signup" className='login-signup-link'>
+            <i className="bi bi-person-plus-fill"></i> Sign up
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
