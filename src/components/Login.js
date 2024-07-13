ea9dbf84c75fa46637a87afa01e0fcbe2482f135
mugetetik <mugetetik00@gmail.com>
2024-07-13 17:08:17 +0300

    fixed routes

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = ({ onLogin }) => {
       alert(res.data.msg);
       UserProfile.setProfile(username, role);
       onLogin(role);
-      const from = location.state?.from?.pathname || '/reserve';
+      const from = location.state?.from?.pathname || '/home';
       navigate(from, { replace: true });
     } catch (err) {
       console.error('Login error:', err);
